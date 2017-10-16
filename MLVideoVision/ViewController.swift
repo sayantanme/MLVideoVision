bc9704e57794bdee02f1698f69b633485d6537ae
Sayantan Chakraborty <saychak2@in.ibm.com>
2017-10-16 12:59:30 +0530

    Initial Commit

---
+++ MLVideoVision/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MLVideoVision
+//
+//  Created by Sayantan Chakraborty on 16/10/17.
+//  Copyright © 2017 Sayantan Chakraborty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
